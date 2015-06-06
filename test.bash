85dcad31b988956e8947fe66256395eb831d12c6
oshio <a1410mo@aiit.ac.jp>
2015-06-06 18:36:54 +0900

    Modification: test code.

--- test.bash
+++ test.bash
@@ -29,11 +29,11 @@ if [ $ans -ne 5 ]; then
 fi
 ans=`./14745110 2 100`
 if [ $ans -ne 2 ]; then
-        ERROR_EXIT "E010: wrong result"
+        ERROR_EXIT "E011: wrong result"
 fi
 ans=`./14745110 67876 3344`
 if [ $ans -ne 4 ]; then
-        ERROR_EXIT "E010: wrong result"
+        ERROR_EXIT "E012: wrong result"
 fi
 
 
