69dd3ef6be6bf895cbe211945c1e8d359c2a7a89
oshio <a1410mo@aiit.ac.jp>
2015-06-06 18:39:40 +0900

    Modification: test way.

--- test.bash
+++ test.bash
@@ -8,18 +8,18 @@ ERROR_EXIT () {
 	exit 1
 }
 
-./14745110
-if [ $? -eq 0 ]; then
+ret='./14745110'
+if [ $ret -eq 0 ]; then
         ERROR_EXIT "E001 : args count check is invalid."
 fi
 
-./14745110 1
-if [ $? -eq 0 ]; then
+ret='./14745110 1'
+if [ $ret -eq 0 ]; then
 	ERROR_EXIT "E002 : arg count check is invalid."
 fi
 
-./14745110 a b
-if [ $? -eq 0 ]; then
+ret='./14745110 a b'
+if [ $ret -eq 0 ]; then
         ERROR_EXIT "E003: arg num check is invalid."
 fi
 
